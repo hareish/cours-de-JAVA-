28a308f24ae1925b98d1a28bb2fca5c7365cc865
hareish <hareish3@gmail.com>
2021-09-27 16:30:38 +0200

    Faire des boucles

---
+++ src/helloworld.java
@@ -0,0 +1,14 @@
+public class helloworld {
+    public static void main(String[] args) {
+            int age = 38;
+            System.out.printf("Mon age %d ans ", age);
+            if (age >= 38)
+            System.out.println("oui je fais plus jeune.");
+
+            int AgeDuCapitaine  =38;
+            if (AgeDuCapitaine  > 20 && age < 50)
+        {
+            System.out.println( "age du capitaine est " +AgeDuCapitaine );
+        }
+    }
+}
